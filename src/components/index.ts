b1a0a2f1a588b15a9fc2383f052634ac3a47be47
rgdlr <rodrigr12@hotmail.com>
2023-04-07 23:40:48 +0200

    feat(components): export modules

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { Card } from "./card";
+export { Cards } from "./cards";
+export { Filter } from "./filter";
+export { Filters } from "./filters";
+export { Search } from "./search";
