b63423ae2518d242b4d746a0fbd9f53566776fb3
rgdlr <rodrigr12@hotmail.com>
2023-04-08 17:27:37 +0200

    feat(hooks): export modules

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,8 @@
 export { useFetch } from "./useFetch";
 export { useI18N } from "./useI18N";
+export { useInterceptor } from "./useInterceptor";
+export { useMocks } from "./useMocks";
+export { useMonkeyPatching } from "./useMonkeyPatching";
 export { useOnBlur } from "./useOnBlur";
 export { useTranslate } from "./useTranslate";
+
