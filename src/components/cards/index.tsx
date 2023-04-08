8595b5457b6807a1a52318b00e33690f72898d85
rgdlr <rodrigr12@hotmail.com>
2023-04-08 19:13:53 +0200

    feat(components): add cards rendering

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,5 +1,12 @@
+import { Card } from "../../components";
 import "./index.css";
 
-export function Cards() {
-	return <article></article>;
+export function Cards({ items }: { items: any }) {
+	return (
+		<ul className="cards">
+			{items?.map((item: any) => (
+				<Card item={item} key={item.id}></Card>
+			))}
+		</ul>
+	);
 }
