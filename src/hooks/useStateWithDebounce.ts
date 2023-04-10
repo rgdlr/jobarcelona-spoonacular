bd8f2b5f5bfec36ee2f5aa081f7d2170efff0345
rgdlr <rodrigr12@hotmail.com>
2023-04-11 00:59:50 +0200

    feat(hooks): modify parameters interface

--- src/hooks/useStateWithDebounce.ts
+++ src/hooks/useStateWithDebounce.ts
@@ -1,19 +1,25 @@
-import { useEffect, useState } from "react";
+import { Dispatch, useEffect, useState } from "react";
 import { useDebounce } from "../hooks";
 
+export interface UseStateWithDebounceOptions<T> {
+	delay?: number;
+	onSetState?: Dispatch<T>;
+	onSetStateWithDebounce?: Dispatch<T>;
+}
+
 export function useStateWithDebounce<T>(
-	initialState: T,
-	delay: number = 500,
-	onSetState?: Function,
-	onSetStateWithDebounce?: Function
+	initialState?: T,
+	options?: UseStateWithDebounceOptions<T>
 ): [
 	stateWithDebounce: typeof stateWithDebounce,
 	setStateWithDebounce: typeof setStateWithDebounce,
 	state: typeof state,
 	setState: typeof setState
 ] {
+	const { delay = 500, onSetState, onSetStateWithDebounce } = options ?? {};
+
 	const [timer, setTimer] = useState<number>();
-	const [state, setState] = useState<T>(initialState);
+	const [state, setState] = useState<T>(initialState as T);
 	const [stateWithDebounce, _setStateWithDebounce] = useState<T>(state);
 
 	const setStateWithDebounce: React.Dispatch<T> = (state: T) => {
@@ -41,16 +47,16 @@ export function useStateWithDebounce<T>(
 
 export function useStateWithUseDebounce<T>(
 	initialState?: T,
-	delay?: number,
-	onSetState?: Function,
-	onSetStateWithDebounce?: Function
+	options?: UseStateWithDebounceOptions<T>
 ): [
 	stateWithDebounce: typeof stateWithDebounce,
 	setStateWithDebounce: typeof setStateWithDebounce,
 	state: typeof state,
 	setState: typeof setState
 ] {
-	const [state, setStateWithDebounce] = useState(initialState);
+	const { delay = 500, onSetState, onSetStateWithDebounce } = options ?? {};
+
+	const [state, setStateWithDebounce] = useState(initialState as T);
 	const [stateWithDebounce, setState] = useDebounce(state, delay);
 
 	useEffect(() => {
