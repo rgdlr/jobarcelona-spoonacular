39b8582311a611a29971547b365aa380fedabd7a
rgdlr <rodrigr12@hotmail.com>
2023-04-11 01:21:59 +0200

    feat(components): improve search behavior

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,27 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Input, Label } from "../../components";
-import { useStateWithDebounce } from "../../hooks";
+import { useOnBlur, useStateWithDebounce } from "../../hooks";
 import "./index.css";
 
-const mockPredictions = [{ text: "yogurt" }, { text: "yellow" }, { text: "yes" }, { text: "no" }];
+interface Autocomplete {
+	id: string;
+	imageType: string;
+	title: string;
+}
 
 export function Search() {
+	const searchRef = useRef(null);
+	const [show, setShow] = useState(false);
 	const [searchWithDebounce, setSearchWithDebounce, search, setSearch] = useStateWithDebounce("");
-	const [predictions, setPredictions] = useState<{ text: string }[]>([]);
+	const [predictions, setPredictions] = useState<Autocomplete[]>([]);
+
+	useOnBlur(searchRef, () => setShow(false), show);
 
 	const updatePredictions = (event: any) => {
-		const updatedPredictions = event.target.value
-			? mockPredictions.filter((prediction) =>
-					prediction.text.toLowerCase().includes(event.target.value.toLowerCase())
-			  )
-			: [];
 		setSearch(event.target.value);
-		setPredictions(updatedPredictions);
+
+		const hasContent = !/^\s*$/g.test(event.target.value);
+		hasContent || setShow(false);
 
 		const url = new URL(window.location.href);
-		!/^\s*$/g.test(event.target.value)
-			? url.searchParams.set("search", event.target.value.trim())
-			: url.searchParams.delete("search");
+		hasContent
+			? url.searchParams.set("query", event.target.value.trim())
+			: url.searchParams.delete("query");
 		window.history.replaceState(null, "", url);
 	};
 
@@ -30,33 +35,47 @@ export function Search() {
 		setSearch(updatedSearch);
 
 		const url = new URL(window.location.href);
-		url.searchParams.set("search", event.target.innerText);
+		url.searchParams.set("query", event.target.innerText);
 		window.history.replaceState(null, "", url);
 	};
 
+	useEffect(() => {
+		if (!searchWithDebounce) {
+			return;
+		}
+		fetch(`recipes/autocomplete?number=5&query=${searchWithDebounce}`)
+			.then((data) => data.json())
+			.then((data) => {
+				setPredictions(data);
+				setShow(Boolean(data?.length));
+			});
+		return () => setPredictions([]);
+	}, [searchWithDebounce]);
+
 	return (
-		<div className="search">
+		<div className="search" ref={searchRef}>
 			<Label htmlFor="search">Search</Label>
 			<Input id="search" onChange={updatePredictions} type="search" value={search}></Input>
-			<svg
-				className="search__icon"
-				aria-labelledby="search"
-				fill="none"
-				height="18"
-				width="18"
-				xmlns="http://www.w3.org/2000/svg">
-				<path
-					d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
-					stroke="currentColor"
-					strokeWidth="1.333"
-					strokeLinecap="round"
-					strokeLinejoin="round"></path>
-			</svg>
-			<ul className="search__predictions">
+			<button className="search__icon">
+				<svg
+					aria-labelledby="search"
+					fill="none"
+					height="18"
+					width="18"
+					xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+						stroke="currentColor"
+						strokeWidth="1.333"
+						strokeLinecap="round"
+						strokeLinejoin="round"></path>
+				</svg>
+			</button>
+			<ul className={`search__predictions search__predictions--${show ? "show" : "hidden"}`}>
 				{predictions.map((prediction) => (
-					<li className="search__prediction" key={prediction.text}>
+					<li className="search__prediction" key={prediction.id}>
 						<button className="search__prediction-button" onMouseDown={updateSearch}>
-							{prediction.text}
+							{prediction.title}
 						</button>
 					</li>
 				))}
