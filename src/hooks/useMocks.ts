3938682849c4d0ce3d13013120f6fb87af8e78bb
rgdlr <rodrigr12@hotmail.com>
2023-04-11 20:27:48 +0200

    feat(mocks): expose mocks for network usage

--- src/hooks/useMocks.ts
+++ src/hooks/useMocks.ts
@@ -4,12 +4,14 @@ export function useMocks() {
 	useInterceptor((input, init) => {
 		const entryPoint = import.meta.env.VITE_SPOONACULAR_ENTRY_POINT;
 		const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY;
+		const isInDevelopmentMode = import.meta.env.MODE === "development";
 
 		if (typeof input !== "string" || input.includes(entryPoint)) {
 			return [input, init];
 		}
 
 		const url = new URL(entryPoint + input);
+		url.hostname = isInDevelopmentMode ? window.location.hostname : url.hostname;
 		url.searchParams.append("apiKey", apiKey);
 
 		return [url, init];
