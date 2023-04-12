a8d85b3b3a694368240ccd564e72acdc5a4cb672
rgdlr <rodrigr12@hotmail.com>
2023-04-12 23:52:41 +0200

    feat(layout): modify header

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
 import "./index.css";
 
 export function Header() {
-	return <header className="header">Spoonacular</header>;
+	return (
+		<header className="header">
+			<img height={24} width={24} src="/assets/jobarcelona.png" alt="logo" />
+			<Link to="/">JOBARCELONA SPOONACULAR</Link>
+		</header>
+	);
 }
