d6259391246a9f7c51f2d3fb021d94605a4f95a1
rgdlr <rodrigr12@hotmail.com>
2023-04-14 22:14:14 +0200

    feat(components): modify filters styles

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -12,25 +12,25 @@ export function Filter(attributes: FilterAttributes<FilterI>): JSX.Element {
 	const { children, className, item, ...restAttributes } = attributes;
 
 	return (
-		<li {...restAttributes} className={computeClassNames("filters__filter", className)}>
+		<li {...restAttributes} className={computeClassNames("filter", className)}>
 			{item?.type === FilterT.Input && (
-				<input className="filters__input" name={item.id} placeholder={item.text}></input>
+				<input className="filter__input" name={item.id} placeholder={item.text}></input>
 			)}
 			{item?.type === FilterT.Checkbox && (
 				<>
-					<input className="filters__input" id={item.id} name={item.id} type="checkbox" />
-					<label className="filters__label" htmlFor={item.id}>
+					<input className="filter__input" id={item.id} name={item.id} type="checkbox" />
+					<label className="filter__label" htmlFor={item.id}>
 						{item.text}
 					</label>
 				</>
 			)}
 			{item?.type === FilterT.Select && (
-				<select className="filters__select" defaultValue={item.id} name={item.id}>
-					<option className="filters__option" disabled value={item.id}>
+				<select className="filter__select" defaultValue={item.id} name={item.id}>
+					<option className="filter__option" disabled value={item.id}>
 						{item.text}
 					</option>
 					{item.value?.map((option) => (
-						<option className="filters__option" key={option} value={option}>
+						<option className="filter__option" key={option} value={option}>
 							{option}
 						</option>
 					))}
--- src/components/filters/index.tsx
+++ src/components/filters/index.tsx
@@ -48,19 +48,19 @@ export function Filters(attributes: FiltersAttributes<FilterI[]>): JSX.Element {
 					<path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
 				</svg>
 			</button>
-			<form onSubmit={onFilter}>
-				<ul
-					{...restAttributes}
-					className={computeClassNames(
-						"filters__list",
-						`filters__list--${show ? "show" : "hidden"}`,
-						className
-					)}>
+			<form
+				className={computeClassNames(
+					"filters__form",
+					`filters__form--${show ? "show" : "hidden"}`,
+					className
+				)}
+				onSubmit={onFilter}>
+				<ul {...restAttributes} className="filters__list">
 					{items && items?.map((item) => <Filter item={item} key={item.id} />)}
 					{children}
 					<Filter>
-						<input className="filters__input" type="reset" name="reset" />
-						<input className="filters__input" type="submit" name="submit" />
+						<input className="filter__input" type="reset" name="reset" />
+						<input className="filter__input" type="submit" name="submit" />
 					</Filter>
 				</ul>
 			</form>
